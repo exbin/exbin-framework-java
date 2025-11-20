3dfab64236a4fd797ff2a31eb27b0c8429efad07
hajdam <hajdam@users.sf.net>
2025-11-20 23:17:32 +0100

    Docking context - 1

--- modules/exbin-framework-docking-layout/src/main/java/org/exbin/framework/docking/layout/EditorFactory.java
+++ modules/exbin-framework-docking-layout/src/main/java/org/exbin/framework/docking/layout/EditorFactory.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.exbin.framework.docking;
+package org.exbin.framework.docking.layout;
 
 import bibliothek.gui.dock.common.MultipleCDockableFactory;
 
