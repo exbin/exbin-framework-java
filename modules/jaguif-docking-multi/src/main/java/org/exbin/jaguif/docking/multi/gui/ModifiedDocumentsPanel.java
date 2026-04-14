bc24ca135bbf864f8f2e4427b1f8c4eb6c904f66
hajdam <hajdam@users.sf.net>
2026-04-14 16:11:30 +0200

    Dropped @author comment

--- modules/jaguif-docking-multi/src/main/java/org/exbin/jaguif/docking/multi/gui/ModifiedDocumentsPanel.java
+++ modules/jaguif-docking-multi/src/main/java/org/exbin/jaguif/docking/multi/gui/ModifiedDocumentsPanel.java
@@ -1,7 +1,7 @@
 package org.exbin.jaguif.docking.multi.gui;
 
 /*
- * Copyright (C) ExBin Project
+ * Copyright (C) ExBin Project, https://exbin.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
